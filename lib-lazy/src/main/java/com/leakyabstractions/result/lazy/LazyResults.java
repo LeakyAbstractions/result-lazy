7b063eac19cf76a91c4e268f60f9ad92ba5429f7
Guillermo Calvo <guillermo@guillermo.dev>
2023-11-06 00:32:24 +0100

    Remove `LazyResults.ofCallable`

--- lib-lazy/src/main/java/com/leakyabstractions/result/lazy/LazyResults.java
+++ lib-lazy/src/main/java/com/leakyabstractions/result/lazy/LazyResults.java
@@ -19,12 +19,10 @@ package com.leakyabstractions.result.lazy;
 import static java.util.Objects.requireNonNull;
 
 import java.util.NoSuchElementException;
-import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import com.leakyabstractions.result.Result;
-import com.leakyabstractions.result.Results;
 
 /**
  * This class consists exclusively of static methods that return lazy {@link Result} instances.
@@ -57,25 +55,5 @@ public class LazyResults {
     public static <S, F> Result<S, F> ofSupplier(Supplier<Result<S, F>> supplier) {
         requireNonNull(supplier, "supplier");
         return new LazyResult<>(supplier);
-    }
-
-    /**
-     * Creates a new lazy result based on the given {@code callable}. If the given {@code callable} produces a success
-     * value, returns a new successful result with it; otherwise returns a new failed result with the exception thrown
-     * by {@code callable}.
-     *
-     * @apiNote The {@code callable} may throw an exception, but it is not allowed to return {@code
-     *     null}. If it does, then the lazy result will relay a {@code NullPointerException} to its caller when/if it
-     *     needs to be evaluated. If the lazy result needs to be evaluated again, it will throw
-     *     {@link NoSuchElementException} (instead of trying to invoke {@code callable} again). This ensures that
-     *     {@code callable} will be invoked at most once.
-     * @param callable the task that produces a success value, or throws an exception if unable to do so
-     * @param <S> the success type of the result
-     * @return the new lazy result
-     * @throws NullPointerException if {@code callable} is {@code null}
-     */
-    public static <S> Result<S, Exception> ofCallable(Callable<S> callable) {
-        requireNonNull(callable, "callable");
-        return new LazyResult<>(() -> Results.ofCallable(callable));
     }
 }
